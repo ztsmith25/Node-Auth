95498df554f0a8b3e669a1af89b346a68a1e144d
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-09-20 21:29:25 -0700

    set up express application

--- server.js
+++ server.js
@@ -15,3 +15,20 @@ var configDB = require('./config/database.js');
 //configuration ==========
 
 mongoose.connect(configDB.url); //connect to the database
+
+//require('./config/passport')(passport); //pass passport for configuration
+
+//set up express application
+app.use(morgan('dev')); //log every request to the console
+app.use(cookieParser()); //read cookies (needed for auth)
+app.use(bodyParser()); //get information from html forms
+
+app.set('view engine', 'ejs'); //set up ejs for templating
+
+//required for passport:
+app.use(session({secret: 'HappyKittens'})); //session secret
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
