379d8a793432c11b334e5399a4f695474d1a1163
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-09-20 21:20:45 -0700

    add modules to server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+//configuration ==========
+
+mongoose.connect(configDB.url); //connect to the database
